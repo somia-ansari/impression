7326f1ab5b5498cf71952351ab0efacc2d15c314
Somia Sohail Ansari <ansarisomia@gmail.com>
2019-11-16 17:56:52 +0500

    state of basic info

--- src/FormComponents/BasicInfo.jsx
+++ src/FormComponents/BasicInfo.jsx
@@ -1,4 +1,4 @@
-import './BasicInfo.css';
+﻿import './BasicInfo.css';
 import { Input } from 'antd';
 import React, { Component } from 'react'
 const { TextArea } = Input;
@@ -24,36 +24,39 @@ export default class BasicInfo extends Component {
       <div className="BasicInfo">
         <h1>Basic Information</h1>
         <form>
-          <input required type="text" placeholder="Your full name here"
+          <input required type="text" placeholder="Your full name here" value={this.state.name}
             onChange={(e) => {
-              this.setState({ name: e.target.name });
+              this.setState({ name: e.target.value });
             }} />
-          <input type="email" placeholder="Your email here"
+          {console.log(this.state.name)}
+          <input type="email" placeholder="Your email here" value={this.state.email}
             onChange={(e) => {
-              this.setState({ email: e.target.email });
+              this.setState({ email: e.target.value });
             }} />
-          <input type="number" placeholder="Your contact number here"
+          <input type="number" placeholder="Your contact number here" value={this.state.numb}
             onChange={(e) => {
-              this.setState({ numb: e.target.numb });
+              this.setState({ numb: e.target.value });
             }} />
-          <input type="number" placeholder="Your WhatsApp number here"
+          <input type="number" placeholder="Your WhatsApp number here" value={this.state.numbWtsap}
             onChange={(e) => {
-              this.setState({ numbWtsap: e.target.numbWtsap });
+              this.setState({ numbWtsap: e.target.value });
             }} />
+
+
           <div style={{ marginBottom: 16 }}>
             <Input size="large"
               addonBefore="Http://" addonAfter=".com"
-              defaultValue="Your LinkedIn profile url here"
+              defaultValue="Your LinkedIn profile url here" value={this.state.linkedIn}
               onChange={(e) => {
-                this.setState({ linkedIn: e.target.linkedIn });
+                this.setState({ linkedIn: e.target.value });
               }} />
           </div>
           <TextArea size="large"
             rows={4} maxLength={80}
-            placeholder="Your Residential Address here"
+            placeholder="Your Residential Address here" value={this.state.address}
             allowClear={true}
             onChange={(e) => {
-              this.setState({ address: e.target.address });
+              this.setState({ address: e.target.value });
             }} />
         </form >
       </div>
